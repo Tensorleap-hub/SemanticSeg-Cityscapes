ecfb536346be8d020a055d363cc04a2d780cd32f
Danielle Ben Bashat <daniellebenba@gmail.com>
2023-12-05 12:29:11 +0200

    update env

--- leap_custom_test.py
+++ leap_custom_test.py
@@ -1,8 +1,16 @@
 import urllib
 from os.path import exists
 from leap_binder import *
+import kili
+from kili import client
+
 
 def check_custom_integration():
+
+    kili = client.Kili(api_key="819023c9-708b-4dd1-82b4-ce90974ff0ab")
+    assets = kili.assets(project_id="clpbb902w01bg087p9o6l0691", skip=0)
+    responses = subset_images()  # get dataset splits
+
     print("Testing")
     model_path = 'models/DeeplabV3.h5'
     if not exists(model_path):
@@ -12,8 +20,8 @@ def check_custom_integration():
             model_path)
     model = tf.keras.models.load_model(model_path)
     idx = 0
-    responses = subset_images()  # get dataset splits
     train_res = responses[0]  # [training, validation, test]
+    json_data = metadata_json_data(idx, train_res)
     image = input_image(idx, train_res)  # get specific image
     mask_gt = ground_truth_mask(idx, train_res)  # get image gt
     y_true_masks = tf.expand_dims(tf.convert_to_tensor(mask_gt), 0)  # convert ground truth bbs to tensor
@@ -30,7 +38,6 @@ def check_custom_integration():
     class_percent = metadata_class_percent(idx, train_res)
     brightness = metadata_brightness(idx, train_res)
     filename_city_dataset = metadata_filename_city_dataset(idx, train_res)
-    json_data = metadata_json_data(idx, train_res)
 
 if __name__ == "__main__":
     check_custom_integration()
